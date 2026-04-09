cda15cf6d515a2b0bd5213fba2b070a412678d84
newbiejy235 <dbankaii1@gmail.com>
2026-04-09 23:59:38 +0700

    repair bug02

--- src/main.js
+++ src/main.js
@@ -15,8 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(middleWare);
 app.use(express.json());
-
-
+app.use(express.static("public"));
 
 app.post("/recovery", async (req, res) => {
   const { email } = req.body;
