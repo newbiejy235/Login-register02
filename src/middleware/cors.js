ae195d2774d9a549ffafb9ff38f255ac76d680d0
newbiejy235 <dbankaii1@gmail.com>
2026-04-13 10:51:44 +0700

    update 02

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "http://localhost:4000",
-    "https://newbiejy235.github.io/LogiinPrev",
+    "https://newbiejy235.github.io",
     "https://login-register02-production-85a9.up.railway.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
